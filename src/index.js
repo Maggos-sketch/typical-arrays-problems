8ca2100ce7472c97d42e27e2c718249cd468d006
Max <68688024+Maggos-sketch@users.noreply.github.com>
2021-03-10 19:52:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  let res;
+  if (array.length == 0) {
+    res = 0;
+  } else {
+    res = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (res > array[i]) {
+        res = array[i];
+      }
+    }
+  }
+  return res;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let res;
+  if (array.length == 0) {
+    res = 0;
+  } else {
+    res = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (res < array[i]) {
+        res = array[i];
+      }
+    }
+  }
+  return res;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let res;
+  if (array.length == 0) {
+    res = 0;
+  } else {
+    res = 0;
+    for (let i = 0; i < array.length; i++) {
+      res += array[i];
+    }
+    res = res / array.length;
+  }
+  return res;
 }
