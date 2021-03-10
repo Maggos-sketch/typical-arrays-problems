b997b18f7b77218e777b570ea4b874a96af2d10d
Max <68688024+Maggos-sketch@users.noreply.github.com>
2021-03-10 20:34:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,55 @@
-
-exports.min = function min (array) {
-  let res;
-  if (array.length == 0) {
-    res = 0;
-  } else {
-    res = array[0];
-    for (let i = 0; i < array.length; i++) {
-      if (res > array[i]) {
-        res = array[i];
-      }
+exports.min = function min(array) {
+    let res;
+    try {
+        if (array.length == 0) {
+            res = 0;
+        } else {
+            res = array[0];
+            for (let i = 0; i < array.length; i++) {
+                if (res > array[i]) {
+                    res = array[i];
+                }
+            }
+        }
+    } catch (e) {
+        res = 0;
     }
-  }
-  return res;
-}
+    return res;
+};
 
-exports.max = function max (array) {
-  let res;
-  if (array.length == 0) {
-    res = 0;
-  } else {
-    res = array[0];
-    for (let i = 0; i < array.length; i++) {
-      if (res < array[i]) {
-        res = array[i];
-      }
+exports.max = function max(array) {
+    let res;
+    try {
+        if (array.length == 0) {
+            res = 0;
+        } else {
+            res = array[0];
+            for (let i = 0; i < array.length; i++) {
+                if (res < array[i]) {
+                    res = array[i];
+                }
+            }
+        }
+    } catch (e) {
+        res = 0;
     }
-  }
-  return res;
-}
+    return res;
+};
 
-exports.avg = function avg (array) {
-  let res;
-  if (array.length == 0) {
-    res = 0;
-  } else {
-    res = 0;
-    for (let i = 0; i < array.length; i++) {
-      res += array[i];
+exports.avg = function avg(array) {
+    let res;
+    try {
+        if (array.length == 0) {
+            res = 0;
+        } else {
+            res = 0;
+            for (let i = 0; i < array.length; i++) {
+                res += array[i];
+            }
+            res = res / array.length;
+        }
+    } catch (e) {
+        res = 0;
     }
-    res = res / array.length;
-  }
-  return res;
-}
+    return res;
+};
